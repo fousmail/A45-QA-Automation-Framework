1a5902810f35eba28c569b3061ecdbcf1f4c35cb
fousmail <130828915+fousmail@users.noreply.github.com>
2023-04-22 03:27:31 -0500

    Homework16
    Homework16

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -19,7 +21,10 @@ public class LoginTests extends BaseTest {
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+
+
 }
