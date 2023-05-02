cea13ce149d39d212b95878360399cbcc0611501
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-02 17:04:48 -0500

    HM18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -5,12 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest {
-
-
     @Test
-
     public void playSongTest () throws InterruptedException {
-
         openLoginUrl();
         enterEmail("faiz.ousmail@testpro.io");
         enterPassword("te$t$tudent1");
