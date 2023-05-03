16093ce3cb58ed7921e366b36f84e06037a33710
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-03 18:03:53 -0500

    HM18
    HM18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,11 +1,8 @@
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 public class Homework18 extends BaseTest {
     @Test
     public void playSongTest () throws InterruptedException {
-
         logIn("faiz.ousmail@testpro.io","te$t$tudent1");
         clickSubmit();
         Thread.sleep(5000);
