af4fdf9e6d271b8c586ceda8143b5c9274467c47
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-03 18:34:34 -0500

    Update

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,7 +1,6 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 public class Homework19 extends BaseTest{
-
     @Test
     public void deletePlaylist() throws InterruptedException {
         String deletedPlaylistMSG = "Deleted playlist";
