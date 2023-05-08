ee4ee06e22633bfd04a1571132f020c0e0b69e4b
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-08 12:40:31 -0500

    05/08/23

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class Homework20 extends BaseTest{
