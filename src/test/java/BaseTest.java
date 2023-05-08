2b72c8ab40e73782c83b686ae943c06abaf597b9
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-08 16:53:40 -0500

    HW21

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -14,6 +16,8 @@ public class BaseTest {
     static WebDriverWait wait;
     String url = "";
     public static WebDriver driver = null;
+    public static Actions actions = null;
+    String newPlaylistName = "Updated playlist";
 
     @BeforeSuite
     static void setupClass() {
@@ -35,6 +39,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         driver = new ChromeDriver(options);
+        actions = new Actions(driver);
         wait = new WebDriverWait(driver,Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -111,5 +116,22 @@ public class BaseTest {
     public String getDeletedPlaylistMSG(){
         WebElement deletionMSG = driver.findElement(By.cssSelector("div.success.show"));
         return deletionMSG.getText();
+    }
+
+    public void doubleClickPlaylist(){
+        WebElement playListElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(playListElement).perform();
+    }
+
+    public void enterNewPlaylistName(){
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.BACK_SPACE));
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public boolean doesPlaylistExist(){
+        WebElement playListElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='"+newPlaylistName+"']")));
+        return playListElement.isDisplayed();
     }
 }
