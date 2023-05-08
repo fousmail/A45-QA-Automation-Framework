c375241937accbb1d2bf4210e49fa4d6587aa73e
fousmail <130828915+fousmail@users.noreply.github.com>
2023-05-08 12:36:48 -0500

    05/08/23

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,14 +1,12 @@
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class Homework19 extends BaseTest{
 
     @Test
-    public void deletePlaylist() throws InterruptedException {
+    public void deletePlaylist() {
         String deletedPlaylistMSG = "Deleted playlist";
         logIn("faiz.ousmail@testpro.io","te$t$tudent1");
         clickSubmit();
