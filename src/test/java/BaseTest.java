4db1d70e93cee327a13c8de29aca10f9bca4b328
fousmail <130828915+fousmail@users.noreply.github.com>
2023-06-06 22:30:07 -0500

    HW24
    HW24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,20 +1,35 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
+    static WebDriverWait wait;
     String url = "";
     public static WebDriver driver = null;
+    public static Actions actions = null;
+    String newPlaylistName = "Updated playlist";
 
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
     }
     @DataProvider(name="InvalidLoginCredentials")
     public static Object[][] getDataFromDataProvider(){
@@ -26,20 +41,61 @@ public class BaseTest {
     }
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void openLoginUrl(String BaseURL) {
+    public void openLoginUrl(String BaseURL) throws MalformedURLException {
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+//        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+//        driver = new FirefoxDriver();
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        actions = new Actions(driver);
+        driver.manage().window().maximize();
         url = BaseURL;
-        driver.get(BaseURL);
+        navigateToPage();
     }
+    public void navigateToPage(){driver.get(url);}
+
     @AfterMethod
     public void closeBrowser() {
         driver.quit();
+    }
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.86.95:4444";
+        switch (browser) {
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            }
+            case "MicrosoftEdge" -> {
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions eOptions = new EdgeOptions();
+                eOptions.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
+                eOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                return driver = new EdgeDriver(eOptions);
+            }
+            case "grid-edge" -> {
+                caps.setCapability("browserName","MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            }
+            case "grid-firefox" -> {
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            }
+            case "grid-chrome" -> {
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            }
+            default -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications", "--remote-allow-origins=*", "--incognito", "--start-maximized");
+                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                return driver = new ChromeDriver(options);
+            }
+        }
     }
     public void openAllSongs(){
         WebElement clickOpenAllSongs = driver.findElement(By.cssSelector("a[href='#!/songs']"));
@@ -55,51 +111,49 @@ public class BaseTest {
     }
     public void logIn(String email, String password) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.click();
+        wait.until(ExpectedConditions.elementToBeClickable((By.cssSelector("input[type='email']")))).click();
         emailField.clear();
         emailField.sendKeys(email);
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='password']"))).click();
         passwordField.clear();
         passwordField.sendKeys(password);
     }
     public void clickSubmit() {
-        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='submit']"))); //driver.findElement(By.cssSelector("[type='submit']"));
         loginButton.click();
     }
-
-    public void searchSong(String songTitleKeyword) throws InterruptedException {
+    public void searchSong(String songTitleKeyword) {
         WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='search']"))).click();
         searchField.sendKeys(songTitleKeyword);
-
-        Thread.sleep(2000);
     }
-    public void clickViewAllButton() throws InterruptedException {
+    public void clickViewAllButton()  {
         WebElement viewAllSearchResults = driver.findElement(By.cssSelector("div.results h1 > button"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.results h1 > button")));
         viewAllSearchResults.click();
-        Thread.sleep(2000);
     }
-    public void selectFirstSongResult() throws InterruptedException {
+    public void selectFirstSongResult() {
         WebElement firstSongResult = driver.findElement(By.cssSelector("section#songResultsWrapper div.song-list-wrap div.virtual-scroller.scroller div.item-container  table.items tr.song-item td.title"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("section#songResultsWrapper div.song-list-wrap div.virtual-scroller.scroller div.item-container  table.items tr.song-item td.title")));
         firstSongResult.click();
-        Thread.sleep(2000);
     }
-    public void clickAddToBtn() throws InterruptedException {
+    public void clickAddToBtn()  {
         WebElement addToBtn = driver.findElement(By.cssSelector("section#songResultsWrapper button.btn-add-to"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("section#songResultsWrapper button.btn-add-to")));
         addToBtn.click();
-        Thread.sleep(2000);
     }
-    public void choosePlaylist() throws InterruptedException {
-        WebElement playListElement = driver.findElement(By.xpath  ("//section[@id='songResultsWrapper']// li[contains(text(),'TEST')]"));
+    public void choosePlaylist()  {
+        WebElement playListElement = driver.findElement(By.xpath("//section[@id='songResultsWrapper']// li[contains(text(),'TEST')]"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//section[@id='songResultsWrapper']// li[contains(text(),'TEST')]")));
         playListElement.click();
-        Thread.sleep(2000);
     }
     public void getNotificationText() {
         WebElement notificationElement = driver.findElement(By.cssSelector("div.success.show"));
         Assert.assertEquals(notificationElement.getText(), "Added 1 song into \"TEST.\"");
     }
     public void openPlaylist(){
-        WebElement playList = driver.findElement(By.cssSelector(".playlist:nth-child(4)"));
+        WebElement playList = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
         playList.click();
     }
     public void clickDeletePlaylistBtn(){
@@ -109,5 +163,22 @@ public class BaseTest {
     public String getDeletedPlaylistMSG(){
         WebElement deletionMSG = driver.findElement(By.cssSelector("div.success.show"));
         return deletionMSG.getText();
+    }
+
+    public void doubleClickPlaylist(){
+        WebElement playListElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(playListElement).perform();
+    }
+
+    public void enterNewPlaylistName(){
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"a",Keys.BACK_SPACE));
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public boolean doesPlaylistExist(){
+        WebElement playListElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='"+newPlaylistName+"']")));
+        return playListElement.isDisplayed();
     }
 }
